2aeac4ad7b03af371e492b63c414606f15d63b81
reki2000 <2533597+reki2000@users.noreply.github.com>
2022-08-27 11:29:25 +0900

    refactored example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math' as math;
 import 'dart:typed_data';
 
@@ -36,12 +37,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late final AudioStream audioStream;
+  AudioStreamStat stat = AudioStreamStat.empty();
 
   @override
   void initState() {
     super.initState();
     audioStream = getAudioStream();
-    audioStream.init();
+    audioStream.init(bufferMilliSec: 1000, waitingBufferMilliSec: 100);
   }
 
   @override
@@ -50,13 +52,52 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  void synthSineWave(double freq, int rate, double sec) async {
-    // for web, calling `resume()` from user-action is needed
-    audioStream.resume();
+  static Float32List synthSineWave(double freq, int rate, Duration duration) {
+    final length = duration.inMilliseconds * rate ~/ 1000;
 
-    final sineWave = List.generate((sec * rate).toInt(),
-        (i) => math.sin(2 * math.pi * ((i * freq) % rate) / rate));
-    audioStream.push(Float32List.fromList(sineWave));
+    final sineWave = List.generate(length, (i) {
+      final amp = math.sin(2 * math.pi * ((i * freq) % rate) / rate);
+
+      const reduceSamples = 100;
+      double reduce;
+      if (i > length - reduceSamples) {
+        reduce = (length - i - 1) / reduceSamples;
+      } else if (i < reduceSamples) {
+        reduce = i / reduceSamples;
+      } else {
+        reduce = 1.0;
+      }
+
+      return amp * reduce;
+    });
+
+    return Float32List.fromList(sineWave);
+  }
+
+  Future<void> play(double freq, Duration duration) async {
+    const hz = 60;
+    const rate = 44100;
+    const step = rate ~/ hz;
+
+    final wave = synthSineWave(freq, rate, duration);
+    int pos = 0;
+
+    final completer = Completer();
+
+    // push pieces of generated wave to audio stream
+    Timer.periodic(const Duration(milliseconds: 1000 ~/ hz), (t) {
+      audioStream.push(wave.sublist(pos, pos + step));
+
+      setState(() => stat = audioStream.stat());
+
+      pos += step;
+      if (pos >= wave.length) {
+        t.cancel();
+        completer.complete();
+      }
+    });
+
+    await completer.future;
   }
 
   @override
@@ -69,10 +110,13 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text("full: ${stat.full} exhaust:${stat.exhaust}"),
             ElevatedButton(
-                onPressed: () {
-                  for (final freq in [261.626, 293.665, 329.628]) {
-                    synthSineWave(freq, 44100, 0.5);
+                onPressed: () async {
+                  // for web, calling `resume()` from user-action is needed
+                  audioStream.resume();
+                  for (double freq in [261.626, 293.665, 329.628]) {
+                    await play(freq, const Duration(seconds: 1));
                   }
                 },
                 child: const Text(
