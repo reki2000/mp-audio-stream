f581efc38b204ec17849514d91af025cca79efad
reki2000 <2533597+reki2000@users.noreply.github.com>
2022-08-27 11:31:05 +0900

    load js source from asset

--- lib/src/mp_audio_stream_web.dart
+++ lib/src/mp_audio_stream_web.dart
@@ -1,81 +1,40 @@
+import 'dart:async';
 import 'dart:js' as js;
 import 'dart:html' as html;
 import 'dart:typed_data';
 
+import 'package:flutter/services.dart';
+
 import '../mp_audio_stream.dart';
 
-String _jsText() => r"""
-(async () => {
-  class AudioWorkletProcessor{}
-
-  class Processor extends AudioWorkletProcessor {
-    constructor() {
-      super();
-      this.maxBufferSize = 1024 * 128;
-      this.keepBufferSize = 1024 * 2;
-      this.buffer = [];
-      this.port.onmessage = (event) => {
-        if (this.buffer.length < this.maxBufferSize) {
-          this.buffer.push(...event.data);
-        }
-      };
-    }
-
-    process(_, outputs, __) {
-      const out = outputs[0][0];
-
-      if (out.length + this.keepBufferSize < this.buffer.length) {
-        for (let i=0; i<out.length; i++) {
-          out[i] = this.buffer[i];
-        }
-        this.buffer = this.buffer.slice(out.length);
-      }
-
-      return true;
-    }
-  }
-
-  var audioCtx;
-  var workletNode;
-
-  window.AudioStream = {
-    init: async (bufSize, waitingBufSize, channels, sampleRate) => {
-      audioCtx = new AudioContext({sampleRate:sampleRate});
-
-      const proc = Processor;
-      const f = `data:text/javascript,${encodeURI(proc.toString())}; registerProcessor("${proc.name}",${proc.name});`;
-      await audioCtx.audioWorklet.addModule(f);
-
-      workletNode = new AudioWorkletNode(audioCtx, 'Processor');
-      workletNode.connect(audioCtx.destination);
-    },
-    resume: async () => {
-      await audioCtx.resume();
-    },
-    push: async (data) => {
-      workletNode.port.postMessage(data);
-    },
-    uninit: async () => {}
-  };
-})();
-""";
+Future<String> _jsText() async =>
+    await rootBundle.loadString('packages/mp_audio_stream/js/audio_stream.js');
 
 /// Contol class for AudioStream on web platform. Use `getAudioStream()` to get its instance.
 class AudioStreamImpl extends AudioStream {
+  final _stream = Completer<js.JsObject>();
+
   AudioStreamImpl() {
-    final scriptTag = html.ScriptElement()..text = _jsText();
-    html.document.head?.children.add(scriptTag);
+    (() async {
+      final scriptTag = html.ScriptElement()..text = await _jsText();
+      html.document.head?.children.add(scriptTag);
+      _stream.complete(js.context['AudioStream']);
+    })();
+  }
+
+  void call(String method, List args) {
+    _stream.future.then((s) => s.callMethod(method, args));
   }
 
   @override
   void resume() {
-    js.context['AudioStream']?.callMethod('resume', []);
+    call('resume', []);
   }
 
   @override
   int push(Float32List buf) {
     // TODO: returns the result for calling `push`
-    js.context['AudioStream']?.callMethod('push', [buf]);
+    call('push', [buf]);
     return 0;
   }
 
@@ -85,7 +44,7 @@ class AudioStreamImpl extends AudioStream {
       int waitingBufferMilliSec = 100,
       int channels = 1,
       int sampleRate = 44100}) {
-    js.context['AudioStream']?.callMethod('init', [
+    call('init', [
       bufferMilliSec * sampleRate / 1000,
       waitingBufferMilliSec * sampleRate / 1000,
       channels,
@@ -96,6 +55,14 @@ class AudioStreamImpl extends AudioStream {
 
   @override
   void uninit() {
-    js.context['AudioStream']?.callMethod('uninit', []);
+    call('uninit', []);
   }
+
+  @override
+  AudioStreamStat stat() {
+    return AudioStreamStat.empty();
+  }
+
+  @override
+  void resetStat() {}
 }
